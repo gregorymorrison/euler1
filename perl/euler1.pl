469846fc89f8fab87425d58e56f02821ec2ab265
gdm9000 <g_is_for_greg@hotmail.com>
2012-02-25 23:05:36 -0500

    Update perl/euler1.pl

---
+++ perl/euler1.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+# Euler1 in Perl
+
+sub euler1 {
+    my @array = (0..shift);
+    my $sum = 0;
+    foreach (@array) {
+        if ($_ % 3 == 0 or $_ % 5 == 0) {
+            $sum += $_;
+        }
+    }
+    return $sum;
+}
+print euler1(999);
