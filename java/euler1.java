2665accc7684db11f1141e5310e45541848ffe11
gdm9000 <g_is_for_greg@hotmail.com>
2012-02-25 22:36:28 -0500

    Update java/euler1.java

---
+++ java/euler1.java
@@ -0,0 +1,31 @@
+// Euler1 in Java
+
+class Euler1 {
+    private int size;
+    private int result;
+
+    public Euler1(int size) {
+        this.size = size;
+    }
+
+    public void solve() {
+        result = 0;
+        for (int i=0; i<size; i++) {
+            if (i%3==0 || i%5==0) {
+                result += i;
+            }
+        }
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Euler1 euler1 = new Euler1(1000);
+
+        euler1.solve();
+
+        System.out.println(euler1.getResult());
+    }
+}
