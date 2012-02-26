4d482627dbaf49c357426b4553ad74a04a018067
gdm9000 <g_is_for_greg@hotmail.com>
2012-02-25 23:13:41 -0500

    Update sql/euler1.sql

---
+++ sql/euler1.sql
@@ -0,0 +1,16 @@
+// Euler1 in C
+#include<stdio.h>
+
+int euler(int n) {
+    int retval = 0;
+    for (int i=0; i<n; i++) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            retval += i;
+        }
+    }
+    return retval;
+}
+
+main() {
+    printf("%i\n", euler(1000));
+}
