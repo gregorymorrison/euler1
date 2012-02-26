5860baf836691ea05b378fdfcc4ebd67bc5b38cb
gdm9000 <g_is_for_greg@hotmail.com>
2012-02-25 23:19:14 -0500

    Update bash/euler1.sh

---
+++ bash/euler1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Euler1 in Bash
+
+euler1() {
+    retval=0
+
+    for i in $(seq $1); do
+        if [ $[$i%3] -eq 0 ]  ||  [ $[$i%5] -eq 0 ]; then
+            retval=$[ $retval+$i ]
+        fi
+    done
+
+    echo $retval
+}
+
+echo $(euler1 999)
