86ffae1207399e2781e7ab795daac6bdd11198cb
gdm9000 <g_is_for_greg@hotmail.com>
2012-02-25 23:10:29 -0500

    Update lua/euler1.lua

---
+++ lua/euler1.lua
@@ -0,0 +1,14 @@
+#!/usr/bin/lua
+-- Euler1 in Lua
+
+function euler(n)
+    local retval = 0
+    for i = 0, n do
+        if i % 3 == 0 or i % 5 == 0 then
+            retval = retval + i
+        end
+    end
+    return retval
+end
+
+print (euler(999))
