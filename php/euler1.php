0c75b6066d3e1a33eb5b55ee31532b415f4e3aa4
gdm9000 <g_is_for_greg@hotmail.com>
2012-02-25 22:36:52 -0500

    Update php/euler1.php

---
+++ php/euler1.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php -q
+<?php// Euler1 in PHP
+
+function euler1($n) {
+    $sum = 0;
+
+    for ($i=0; $i<$n; $i++) {
+        if ($i%3 == 0 || $i%5==0) {
+            $sum += $i;
+        }
+    }
+    return $sum;
+}
+
+$result = euler1(1000);
+?>
+Euler1 = <?php echo $result . "\n" ?>
