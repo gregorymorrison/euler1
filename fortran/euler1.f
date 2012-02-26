6747527862964bf6f246941d816878a4c6346c24
gdm9000 <g_is_for_greg@hotmail.com>
2012-02-25 22:24:33 -0500

    Update fortran/euler1.f

--- fortran/euler1.f
+++ fortran/euler1.f
@@ -1,1 +1,18 @@
-df
+! Euler1 in Fortran90
+
+program euler1
+    integer :: euler
+
+    print*, euler(999)
+end
+
+function euler(size)
+    integer euler, size
+    euler = 0
+
+    do i = 0, size
+        if (modulo(i,3)==0 .or. modulo(i,5)==0) then
+             euler = euler + i
+        end if
+ end do
+end
