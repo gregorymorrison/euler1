46057cfd9a5a46e63e24f31351cfb7ffba1bf87d
Gregory Morrison <greg.morrison@gmail.com>
2019-11-28 15:15:55 -0500

    Getting rid of case-sensitive issues.

--- cobol/Euler1.cob
+++ cobol/Euler1.cob
@@ -4,8 +4,8 @@
        PROGRAM-ID. myTest.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01  cnt      PIC 9(4) VALUE 999.
-       01  result   PIC 9(9).
+       01  cnt     PIC 9(4) VALUE 999.
+       01  result  PIC 9(9).
        PROCEDURE DIVISION.
            CALL "euler1" USING BY CONTENT cnt, 
                                BY REFERENCE result.
@@ -17,24 +17,24 @@
        PROGRAM-ID. euler1.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01  quot     PIC 9(3) VALUE 0.
-       01  rem      PIC 9(3) VALUE 0.
+       01  quot PIC 9(3) VALUE 0.
+       01  rem  PIC 9(3) VALUE 0.
        LINKAGE SECTION.
-       01  cnt      PIC 9(4).
-       01  result   PIC 9(9) VALUE 0.
+       01  cnt     PIC 9(4).
+       01  result  PIC 9(9) VALUE 0.
        PROCEDURE DIVISION USING cnt, result.
-           PERFORM UNTIL cnt <= 0 
+           PERFORM UNTIL cnt <= 0
                DIVIDE 3 INTO cnt GIVING quot REMAINDER rem
                IF rem = 0 THEN
                    ADD cnt TO result
                ELSE
                    DIVIDE 5 INTO cnt GIVING quot REMAINDER rem
                    IF rem = 0 THEN
-                       ADD cnt TO result
-                   END-IF
-               END-IF
+                       ADD cnt TO result 
+                    END-IF 
+                END-IF
 
                ADD -1 TO cnt
-           END-PERFORM
+           END-PERFORM 
        EXIT PROGRAM.
        END PROGRAM euler1.
--- java/Euler1.java
+++ java/Euler1.java
@@ -1,33 +1,31 @@
 // Euler1 in Java
 
 class Euler1 {
-	private int size;
-	private int result;
+    private int size;
+    private int result;
 
-	public Euler1(int size) {
-		this.size = size;
-	}
+    public Euler1(int size) {
+        this.size = size;
+    }
 
-	public void solve() {
-		result = 0;
-		for (int i=0; i<size; i++) {
-			if (i%3==0 || i%5==0) {
-				result += i;
-			}
-		}
-	}
+    public void solve() {
+        result = 0;
+        for (int i=0; i<size; i++) {
+            if (i%3==0 || i%5==0) {
+                result += i;
+            }
+        }
+    }
 
-	public int getResult() {
-		return result;
-	}
+    public int getResult() {
+        return result;
+    }
 
-	public static void main(String[] args) {
-		Euler1 euler1 = new Euler1(1000);
+    public static void main(String[] args) {
+        Euler1 euler1 = new Euler1(1000);
 
-		euler1.solve();
+        euler1.solve();
 
-		System.out.println(euler1.getResult());
-	}
+        System.out.println(euler1.getResult());
+    }
 }
-
-
