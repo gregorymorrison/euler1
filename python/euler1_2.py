2632d7294ee3674ca505110cdc178a8e75006f98
Gregory Morrison <greg.morrison@gmail.com>
2023-07-08 18:51:31 -0400

    Added more rust examples.

--- python/euler1_2.py
+++ python/euler1_2.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 # Euler1 in Python
 
-def euler1(n):
+def euler(n):
     i = 0
     while i < n:
         if i%3==0 or i%5==0:
             yield i
         i += 1
 
-print ("Euler1 =", sum(euler1(1000)))
+def euler1(n):
+    return sum(euler(n))
+
+print ("Euler1 =", euler1(1000))
